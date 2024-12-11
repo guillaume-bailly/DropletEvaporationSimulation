b38fafca0f48ff4a274c7d030c5f7e89837806cf
Guillaume Bailly <guillaume.bailly@estaca.eu>
2024-12-11 18:11:29 +0100

    change path to src

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../src"))
 
 project = "Droplet Evaporation Simulation"
 copyright = "2024, Guillaume Bailly"
