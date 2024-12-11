c0278eefeab4340804c725c21cb18e5349d8a944
Guillaume Bailly <guillaume.bailly@estaca.eu>
2024-12-11 21:13:12 +0100

    troubleshooting

--- docs/conf.py
+++ docs/conf.py
@@ -11,8 +11,6 @@ import sys
 sys.path.insert(0, os.path.abspath(".."))
 
 
-extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
-
 project = "Droplet Evaporation Simulation"
 copyright = "2024, Guillaume Bailly"
 author = "Guillaume Bailly"
@@ -21,7 +19,8 @@ release = "1.0.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
+# extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
+extensions = []
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
