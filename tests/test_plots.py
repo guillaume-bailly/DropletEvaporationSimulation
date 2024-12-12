224b228c0a5fdc020a8627634a0956f1ce80c2fd
Guillaume Bailly <guillaume.bailly@estaca.eu>
2024-12-12 20:27:52 +0100

    new assert (new fig generated)

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -15,7 +15,7 @@ from plots import (
     plot_droplet_position,
 )
 from utils import interpolate_ode
-from scipy.integrate import cumtrapz, solve_ivp
+from scipy.integrate import cumulative_trapezoid, solve_ivp
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -23,15 +23,19 @@ import matplotlib.pyplot as plt
 def test_plot_velocity():
     time = np.linspace(0, 4, 100)
     velocity = 2 * np.exp(-10 * time)
-    fig = plot_velocity(time, velocity)
-    assert isinstance(fig, plt.Figure), "Should return a matplotlib figure"
+    initial_fig = plt.gcf()
+    plot_velocity(time, velocity)
+    final_fig = plt.gcf()
+    assert initial_fig != final_fig, "A new matplotlib figure should have been created."
 
 
 def test_plot_diameter_squared():
     time = np.linspace(0, 4, 100)
     diameter = time * 0.5
-    fig = plot_diameter_squared(time, diameter, "Test Plot")
-    assert isinstance(fig, plt.Figure), "Should return a matplotlib figure"
+    initial_fig = plt.gcf()
+    plot_diameter_squared(time, diameter, "Test Plot")
+    final_fig = plt.gcf()
+    assert initial_fig != final_fig, "A new matplotlib figure should have been created."
 
 
 def test_plot_droplet_temperature():
@@ -41,32 +45,38 @@ def test_plot_droplet_temperature():
     tspan = [0, 5]  # defining time interval
     Tp0 = [300]  # initial condition
     droplet_temperature = solve_ivp(interpolate_ode, tspan, Tp0, args=(time, f, g))
-    fig = plot_droplet_temperature(droplet_temperature)
-    assert isinstance(fig, plt.Figure), "Should return a matplotlib figure"
+    initial_fig = plt.gcf()
+    plot_droplet_temperature(droplet_temperature)
+    final_fig = plt.gcf()
+    assert initial_fig != final_fig, "A new matplotlib figure should have been created."
 
 
 def test_plot_droplet_velocity():
     ta = np.linspace(0, 4, 100)
-    f = 2 * time + 4  # test function
-    g = 5 * time - 6  # test function
+    f = 2 * ta + 4  # test function
+    g = 5 * ta - 6  # test function
     tspan = [0, 5]  # defining time interval
     Up0 = [300]  # initial condition
     Up = solve_ivp(interpolate_ode, tspan, Up0, args=(ta, f, g))
     time = Up.t[:400]
     velocity = Up.y[0][:400]
-    fig = plot_droplet_velocity(time, velocity, "Test Plot")
-    assert isinstance(fig, plt.Figure), "Should return a matplotlib figure"
+    initial_fig = plt.gcf()
+    plot_droplet_velocity(time, velocity, "Test Plot")
+    final_fig = plt.gcf()
+    assert initial_fig != final_fig, "A new matplotlib figure should have been created."
 
 
 def test_plot_droplet_position():
     ta = np.linspace(0, 4, 100)
-    f = 2 * time + 4  # test function
-    g = 5 * time - 6  # test function
+    f = 2 * ta + 4  # test function
+    g = 5 * ta - 6  # test function
     tspan = [0, 5]  # defining time interval
     Up0 = [300]  # initial condition
     Up = solve_ivp(interpolate_ode, tspan, Up0, args=(ta, f, g))
     time = Up.t[:400]
     velocity = Up.y[0][:400]
-    position = cumtrapz(velocity, time, initial=0)
-    fig = plot_droplet_position(time, position, "Test Plot")
-    assert isinstance(fig, plt.Figure), "Should return a matplotlib figure"
+    position = cumulative_trapezoid(velocity, time, initial=0)
+    initial_fig = plt.gcf()
+    plot_droplet_position(time, position, "Test Plot")
+    final_fig = plt.gcf()
+    assert initial_fig != final_fig, "A new matplotlib figure should have been created."
