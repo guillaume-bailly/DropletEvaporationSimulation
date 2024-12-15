aa7eaa21f9a6c6b5e35d40f03707aa28ef9057cc
Guillaume Bailly <guillaume.bailly@estaca.eu>
2024-12-15 14:43:30 +0100

    new entry point path

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "des_run_simulation = main",
+            "des_run_simulation = dropletevaporationsimulation.src.main:main",
         ],
     },
     classifiers=[
